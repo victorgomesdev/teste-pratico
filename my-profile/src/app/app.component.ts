65949aadb5ec2a2a714720c635a74e09b85b77ad
Victor Gomes Nogueira <victorgomesnog123@gmail.com>
2025-07-04 00:22:32 -0300

    chore: Adiciona tailwind e refatora alguns arquivos no frontend.

--- my-profile/src/app/app.component.ts
+++ my-profile/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from '@common/layout/layout.component';
+
+const IMPORTS = [
+  LayoutComponent
+]
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [...IMPORTS],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-profile';
