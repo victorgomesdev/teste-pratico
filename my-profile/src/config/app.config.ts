125a7826f9b5d8632fdb876beca97edb1350b498
Victor Gomes Nogueira <victorgomesnog123@gmail.com>
2025-07-10 02:27:04 -0300

    chore: Adiciona alguns pacotes.

--- my-profile/src/config/app.config.ts
+++ my-profile/src/config/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { providePrimeNG } from 'primeng/config'
+import Aura from '@primeng/themes/aura';
 
 import { routes } from '@config/routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ]
 };
